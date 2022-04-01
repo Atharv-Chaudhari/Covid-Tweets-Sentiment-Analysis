c2a37b3bb1d4014327fcecbf7f28f5ab462ef984
Atharv-Chaudhari <ahc382000@gmail.com>
2022-04-01 21:52:08 +0530

    deploy app

--- app.py
+++ app.py
@@ -7,8 +7,18 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 nltk.download('stopwords')
+from flask import Flask, render_template, request, send_from_directory, after_this_request, Response, stream_with_context, make_response
+import os
+import sys
 
-app = Flask(__name__)
+
+root_path = os.path.abspath(__file__)
+root_path = '/'.join(root_path.split('/')[:-2])
+sys.path.append(root_path)
+
+
+app = Flask(__name__, static_folder="../static",
+            template_folder="../templates")
 
 @app.route("/")
 def home():
